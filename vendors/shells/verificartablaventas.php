47e649b2282a755f9132b3ba7ac04ae998f948d0
unknown <Administrador@.(none)>
2011-03-20 18:29:08 -0500

    previo al pull

--- vendors/shells/verificartablaventas.php
+++ vendors/shells/verificartablaventas.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * 
+ * Evalua todas las ventas que estan en estado pendiente_de_pago, si ya han
+ * pasado los dias de espera (campo dias_espera tabla subastas) la venta pasa
+ * a estado no_pagada y envia correo a los webmasters del sitio.
+ * 
+ * @author Julio César Domínguez Giraldo
+ *
+ */
 class VerificarTablaVentasShell extends Shell {
 	 
 	var $uses = array('Venta');
