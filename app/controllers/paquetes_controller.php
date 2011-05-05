63d0a1c80388fbd61728c490ba46b9e45243df63
Julio Cesar Dominguez Giraldo <juliodominguez@bloomtec.com>
2011-05-04 21:29:22 -0500

    CRUD de paquetes modificado para ir acorde al sitio, listaado en el menu administrativo. Falta hacer la parte de adquirir; metodo y vista creados pero en blanco.

--- app/controllers/paquetes_controller.php
+++ app/controllers/paquetes_controller.php
@@ -15,49 +15,11 @@ class PaquetesController extends AppController {
 		}
 		$this->set('paquete', $this->Paquete->read(null, $id));
 	}
-
-	function add() {
-		if (!empty($this->data)) {
-			$this->Paquete->create();
-			if ($this->Paquete->save($this->data)) {
-				$this->Session->setFlash(__('The paquete has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The paquete could not be saved. Please, try again.', true));
-			}
-		}
+	
+	function adquirir($id = null) {
+		//TODO : procedimiento para adquirir el paquete
 	}
 
-	function edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid paquete', true));
-			$this->redirect(array('action' => 'index'));
-		}
-		if (!empty($this->data)) {
-			if ($this->Paquete->save($this->data)) {
-				$this->Session->setFlash(__('The paquete has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The paquete could not be saved. Please, try again.', true));
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->Paquete->read(null, $id);
-		}
-	}
-
-	function delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(__('Invalid id for paquete', true));
-			$this->redirect(array('action'=>'index'));
-		}
-		if ($this->Paquete->delete($id)) {
-			$this->Session->setFlash(__('Paquete deleted', true));
-			$this->redirect(array('action'=>'index'));
-		}
-		$this->Session->setFlash(__('Paquete was not deleted', true));
-		$this->redirect(array('action' => 'index'));
-	}
 	function admin_index() {
 		$this->Paquete->recursive = 0;
 		$this->set('paquetes', $this->paginate());
