013ba9c13aeee962ee5181978196c835944c4c8d
Ricardo <ricardopandales@gmail.com>
2011-05-31 11:44:44 -0500

    cola

--- app/webroot/js/admin.js
+++ app/webroot/js/admin.js
@@ -5,3 +5,35 @@ $(function() {
 	$('ul.nav').superfish();
 	
 });
+$(function(){ // Order and reorder
+  var sendData=function(order,controller){
+    var data={};
+    for(i=0;i<order.length;i+=1){
+      data["data[Item]["+order[i]+"]"]=(i+1);
+    }
+    $.post(server+controller+"/reOrder",
+        data,
+        function(response){
+          if(response=="yes"){
+            for(i=0;i<order.length;i+=1){
+              $("tr#"+order[i]).children(".order").text(i+1);
+            }
+          }
+        }
+    );
+    
+    }
+    $( "#sortable tbody" ).sortable({
+      revert: true,
+      items:"tr:not(.ui-state-disabled)",
+      update:function(event, ui){
+    
+      sendData($(this).sortable("toArray"),$("table").attr("controller"));
+      
+      
+      }
+        
+    });
+
+    $( "#sortable tbody > tr" ).disableSelection();
+});
