09b096d7d4797d3854eb85cda07603b0f18ce2b0
pandales <ricardopandales@gmail.com>
2011-06-22 14:35:21 -0500

    integrado con madmimi

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -12,11 +12,6 @@ class UsersController extends AppController {
 		parent::beforeFilter();
 		//$this->Auth->deny("login","abonarCreditosPorRecomendacion","checkEmail","register","checkPassword","rememberPassword","reponerCreditos","creditosUsuario","creditosSuficientes","descontarCreditos");
 	}
-	function prueba(){
-		    App::import('Vendor', 'MadMimi', array('file' =>'madmimi'.DS.'MadMimi.class.php'));
-			$MadmimiApi= new MadMimi();
-		
-	}
 
 	function index() {
 		//debug($this->User->read(null,$this->Auth->user("id")));
@@ -255,6 +250,10 @@ class UsersController extends AppController {
 					}*/
 					
 				//$rol=$this->Session->read("Auth.User.role_id");
+				   App::import('Vendor', 'MadMimi', array('file' =>'madmimi'.DS.'MadMimi.class.php'));
+					$mailer= new MadMimi(Configure::read('madmimiEmail'),Configure::read('madmimiKey'));
+					$userMimi = array('email' => $this->data['User']['email'], 'firstName' => $this->data['UserField']['nombres'], 'lastName' => $this->data['UserField']['apellidos'], 'add_list' => 'cuentas-creadas');
+					$mailer->AddUser($userMimi);
 				$this->Session->setFlash(__('Su registro ha sido éxitoso', true));
 				$this->Auth->login($this->data);
 				$this->Cookie->write("registrado", true);
