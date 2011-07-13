78e0e2bfd34aec039007c1feb1de991c14921a84
Julio Cesar Dominguez Giraldo <juliodominguez@bloomtec.com>
2011-07-13 14:54:25 -0500

    algunos cambios en app_controller

--- app/app_controller.php
+++ app/app_controller.php
@@ -3,33 +3,21 @@
 class AppController extends Controller {
 
 	var $components = array(
-							"Auth" => array(
-											'authorize' => 'controller'
-										),
-							"Session",
-							"Acl",
-							"Email",
-							"Cookie",
-						
-						);
+		"Auth",
+		"Session",
+		"Acl",
+		"Email",
+		"Cookie",
+	);
 	var $uses = array("Config");
 	var $cacheAction = true;
-	
-	public function isAuthorized() {
-		return true;
-	}
 
 	function beforeFilter(){
 		$this->Auth->allow("*");
-		// Este comando de fields no enviaba la contraseña encriptada!!!!
-		// Por eso lo comente -> Julio
-		//$this->Auth->fields = array('username' => 'email', 'password' => 'password');
-		$this->Auth->loginAction = array('controller'=>'users','action'=>'login');
-		$this->Auth->loginRedirect= array('controller'=>'users','action'=>'index');
-		$this->Auth->logoutRedirect= array('controller'=>'subastas','action'=>'index',"admin"=>false);
-		$this->Auth->loginError = "Usuario o Password no válido";
-		$this->Auth->authError = "No tiene permiso para ingresar a la cuenta.";
-		$this->Auth->actionPath = 'controllers/';
+		$this->Auth->loginRedirect = array('controller'=>'users', 'action'=>'index');
+		$this->Auth->logoutRedirect = array('controller'=>'subastas', 'action' => 'index', "admin" => false);
+		$this->Auth->loginError = "Usuario o contraseña no válidos";
+		$this->Auth->authError = "No tiene permiso para ingresar a esta sección.";
 		if(isset($this->params["prefix"]) && $this->params["prefix"]=="admin"){
 			$this->layout="admin";
 			$this->Auth->deny($this->action);
@@ -37,15 +25,13 @@ class AppController extends Controller {
 		$this->Cookie->name = 'Llevatelos';
 		$this->Cookie->time = '10 Days'; // or '1 hour'
 		$this->Cookie->path = '/';
-		   // $this->Cookie->domain = 'priceshoes.com';
-		$this->Cookie->domain = 'localhost/subastas';
+		$this->Cookie->domain = 'www.llevatelos.com';
 		$this->Cookie->secure = false; //i.e. only sent if using secure HTTPS
 		$this->Cookie->key = 'qSI232qs*&sXOw!';
-		
 	}
 
 	function beforeRender(){
-		$PAGE_TITLE="llevatelos.com";
+		$PAGE_TITLE="Llévatelos :: ";
 		$this->set(compact("PAGE_TITLE"));
 		$this->set('base_url', 'http://'.$_SERVER['SERVER_NAME'].Router::url('/'));
 	}
