df655558b260b61d7a218b57ae64dd57fd43c1b4
Ricardo Pandales <ricardopandales@gmail.com>
2011-09-05 18:45:15 -0500

    cambios para lo del iva

--- app/models/subasta.php
+++ app/models/subasta.php
@@ -68,6 +68,18 @@ class Subasta extends AppModel {
 	//'required' => false,
 	//'last' => false, // Stop validation after this rule
 	//'on' => 'create', // Limit validation to 'create' or 'update' operations
+	), ), 'iva' => array('numeric' => array('rule' => array('numeric'),
+	//'message' => 'Your custom message here',
+	//'allowEmpty' => false,
+	//'required' => false,
+	//'last' => false, // Stop validation after this rule
+	//'on' => 'create', // Limit validation to 'create' or 'update' operations
+	), 'notempty' => array('rule' => array('notempty'),
+	//'message' => 'Your custom message here',
+	//'allowEmpty' => false,
+	//'required' => false,
+	//'last' => false, // Stop validation after this rule
+	//'on' => 'create', // Limit validation to 'create' or 'update' operations
 	), ), 'precio' => array('numeric' => array('rule' => array('numeric'),
 	//'message' => 'Your custom message here',
 	//'allowEmpty' => false,
