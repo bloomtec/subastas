711762e4aae79bbd139f0febcfaa865af56fe319
pandales <ricardopandales@gmail.com>
2011-09-15 11:34:03 -0500

    subastas.js comentariado

--- app/models/subasta.php
+++ app/models/subasta.php
@@ -20,7 +20,7 @@ class Subasta extends AppModel {
 	//'required' => false,
 	//'last' => false, // Stop validation after this rule
 	//'on' => 'create', // Limit validation to 'create' or 'update' operations
-	), ), 'descripcion' => array('notempty' => array('rule' => array('notempty'),
+	), ), 
 	//'message' => 'Your custom message here',
 	//'allowEmpty' => false,
 	//'required' => false,
