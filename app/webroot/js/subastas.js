190714c37f6de951555ea32336cbe55c9ba38503
panda <ricardopandales@gmail.com>
2011-09-20 00:22:42 -0500

    monitoreo duracion getStatus

--- app/webroot/js/subastas.js
+++ app/webroot/js/subastas.js
@@ -102,7 +102,7 @@ $(function(){
 	});
 	setInterval(function(){
 		jQuery.ajax({
-			url:server+"subastas/getStatus?ms="+new Date().getTime(),
+			url:server+"subastas/pruebas2?ms="+new Date().getTime(),
 			type: "POST",
 			cache: false,
 			dataType:"json",
