f5f77d9752404672841d7f4feebf717155a7ebe3
Julio C. Dominguez <juliodominguez@gmail.com>
2011-09-19 21:40:22 -0500

    correccion bug factura::generarCodigoFactura()

--- app/controllers/facturas_controller.php
+++ app/controllers/facturas_controller.php
@@ -21,12 +21,12 @@ class FacturasController extends AppController {
 	function generarCodigoFactura() {
 		//$max_codigo_factura = $this -> Factura -> query("SELECT MAX(`codigo_factura`) FROM `facturas` as Factura");
 		$max_codigo_factura = $this -> Factura -> find('first', array('fields' => array('MAX(Factura.codigo_factura) as max_code')));
-		if(!empty($max_codigo_factura)) {
+		if(!empty($max_codigo_factura['0']['max_code'])) {
 			debug('no esta vacio');
-			debug($max_codigo_factura);
+			debug($max_codigo_factura['0']['max_code']);
 		} else {
 			debug('esta vacio');
-			debug($max_codigo_factura);
+			debug($max_codigo_factura['0']['max_code']);
 		}
 		//$codigo = $max_codigo_factura + 1;
 		return 0;
